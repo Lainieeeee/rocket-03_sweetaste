92cfdf63a477fd6d2a398ff87eeb8381142fbb97
Lainieeeee <yorin0820@gmail.com>
2025-03-04 12:02:46 +0800

    feat：create top page,update header/footer

---
+++ assets/js/top.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#topProductsSlider', {
+        type: 'slide',
+        autoplay: false,
+        interval: 3000,
+        arrows: false,
+        pagination: false,
+        perPage: 3,
+        gap: '20px',
+        breakpoints: {
+            1199.98: {        // ~991px
+                type: 'slide', 
+                arrows: true,
+                perPage: 2
+            },
+            767.98: {        // ~767px
+                type: 'slide', 
+                arrows: true,
+                perPage: 1
+            }
+        }
+    }).mount();
+});
